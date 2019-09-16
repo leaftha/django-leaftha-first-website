9d7bca0c5ea0a8efee3a864a362ac2951a50e177
리프 <vltpcks@gmail.com>
2019-09-16 11:29:52 +0900

    blog post list (index)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+
+def index(requst):
+    posts = Post.objects.all()
+    return render(
+        requst,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+
+    )
