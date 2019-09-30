09c9027d3a9d48c426a5f123525d9d68835bafaf
리프 <vltpcks@gmail.com>
2019-09-30 14:55:21 +0900

    category in post_list

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목 표시 글자수 제한 30글자
@@ -10,6 +20,8 @@ class Post(models.Model):
     created= models.DateTimeField() #글 작성 시간
 
     author = models.ForeignKey(User, on_delete=True) #글 작성 유저
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self): #포스터의 제목 결정
         return '{} :: {}'.format(self.title, self.author)
--- blog/tests.py
+++ blog/tests.py
@@ -1,18 +1,60 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-def create_post(title, content, author):
+
+
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+
+    return category
+
+
+def create_post(title, content, author, category=None):
     blog_post = Post.objects.create(
         title= title,
         content= content,
         created=timezone.now(),
         author=author,
+        category = category,
     )
 
     return blog_post
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='leaftha', password='nopassworld')
+
+    def test_category(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world',
+            author=self.author_000,
+            category=category
+        )
+
+        self.assertEqual(category.post_set.count(), 1)
+
+    def test_post(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world',
+            author=self.author_000,
+            category=category
+        )
+
+
 
 # Create your tests here.
 class Testview(TestCase):
@@ -25,7 +67,7 @@ class Testview(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About me', navbar.text)
 
-    def test_post_list(self):
+    def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
@@ -40,11 +82,18 @@ class Testview(TestCase):
         self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
 
-
+    def test_post_list_with_post(self):
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world',
             author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The Second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='정치/사회'),
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -56,12 +105,23 @@ class Testview(TestCase):
         self.assertNotIn('아직 게시물이 없습니다.', body.text)
         self.assertIn(post_000.title, body.text)
 
+
+        # category card 에서
+        category_card = body.find('div', id = 'category-card')
+        self.assertIn('미분류 (1)', category_card.text)   # 미분류 (1)이 있어야한다.
+        self.assertIn('정치/사회 (1)', category_card.text)   # 정치/사회 (1) 있어야 함
+
+        main_div = body.find('div', id ='main_div')
+        self.assertIn('정치/사회', main_div.text)   # 첫번째 포스트에는 정치/사회 있어야함
+        self.assertIn('미분류', main_div.text)   # 두번째 포스트에는 미분류 있어야함
+
     def test_post_detail(self):
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world',
             author=self.author_000,
         )
+
 
         self.assertGreater(Post.objects.count(), 0)
         post_000_url = post_000.get_absolute_url()
